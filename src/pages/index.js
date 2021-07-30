0bdf9be874a51759ff8b4917dd28d4deaaaab661
iamshour-Git <aliahmadshour@gmail.com>
2021-07-30 11:59:39 -0700

    .

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Hero from "../components/Hero"
 import Services from "../components/Services"
 import About from "../components/About"
 import Projects from "../components/Projects"
-import Contact from "../components/contact"
+import Contact from "../components/Contact"
 
 const IndexPage = () => (
   <Layout>
